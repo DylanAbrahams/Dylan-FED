a495d74e2571cd0efdd715d31dfc6cee2596e049
DylanAbrahams <143963818+DylanAbrahams@users.noreply.github.com>
2023-09-20 16:12:55 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+
+var menuButton = document.querySelector("header > button");
+var navigatie = document.querySelector("nav");
+menuButton.onclick = openMenu;
+
+
+function openMenu() {
+  navigatie.classList.add("toonMenu");
+}
+
+
+var closeButton = document.querySelector("nav button")
+closeButton.onclick = closeMenu;
+
+
+function closeMenu() {
+  navigatie.classList.remove("toonMenu")
+}
