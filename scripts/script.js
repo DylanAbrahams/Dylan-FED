6deb33633dc0be43b8766a4513cadad1d3cfbea4
DylanAbrahams <143963818+DylanAbrahams@users.noreply.github.com>
2023-10-04 16:08:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 console.log("hi");
 
 
+// NAVIGATIE OP MOBIEL
 var menuButton = document.querySelector("header > button");
 var navigatie = document.querySelector("nav");
 menuButton.onclick = openMenu;
@@ -28,12 +29,32 @@ function closeMenu() {
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 10) { 
-        console.log("kleur")
-        header.classList.add("scrolled");
-    } else {
-      console.log("transparent")
-        header.classList.remove("scrolled");
-    }
+  if (window.scrollY > 10) {
+    console.log("kleur")
+    header.classList.add("scrolled");
+  } else {
+    console.log("transparent")
+    header.classList.remove("scrolled");
+  }
 });
 
+
+// Bullied Button animatie
+
+const imageContainer = document.querySelector('main section:nth-of-type(1) img');
+const toggleButton = document.querySelector('main section:nth-of-type(1) button');
+
+let animationPaused = false;
+
+
+
+toggleButton.addEventListener('click', () => {
+    if (!animationPaused) {
+        imageContainer.style.animationPlayState = 'running';
+        toggleButton.textContent = 'Pause Animation';
+    } else {
+        imageContainer.style.animationPlayState = 'paused';
+        toggleButton.textContent = 'Play Animation';
+    }
+    animationPaused = !animationPaused;
+});
